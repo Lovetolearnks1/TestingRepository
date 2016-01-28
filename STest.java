2244ecba85de2fc424fdbcf42945f3f085a597a9
Lovetolearnks1 <vaniks.shimoga@gmail.com>
2016-01-27 23:14:06 -0500

    First commit

---
+++ STest.java
@@ -0,0 +1,47 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class STest {
+
+    	public static void main(String[] args) {
+    	
+    	List<HashMap<String, String>> msgContentList;
+    	msgContentList = new ArrayList<HashMap<String, String>>();
+		String message    = "message: {\"emails\":[{\"email\":\"I know you \"can\" do the work. Just get started!\"},{\"email\":\"How is the assignment coming along. I look forward to the update.\"},{\"email\":\"I like the progress you have made. Now I need you to see it through to the end.\"}], \"success\":1}";
+
+		Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+		Matcher matcher = pattern.matcher(message);
+		int i =0;
+		if (matcher.find())
+		{
+		    //System.out.println(matcher.group(1));
+			pattern = Pattern.compile("\\{\"email\":\"(.*?)\"\\}");
+			matcher = pattern.matcher(matcher.group(1));
+			while (matcher.find()) {
+				//System.out.println(matcher.group(1));
+				if (matcher.group(1)!=null && matcher.group(1) != "" &&
+                            !matcher.group(1).equals("null") && matcher.group(1).trim().length() > 0) {
+				//Pattern patternNew = Pattern.compile("\"(.*?)\"");
+				//Matcher matcherNew = patternNew.matcher(matcher.group(1));
+				//if(matcherNew.find()){
+				//	msgContentList.add(i,new HashMap<String, String>()); //v
+				//	msgContentList.get(i).put("name", matcherNew.group(1));
+				//	i++;
+				//}
+					System.out.println(matcher.group(1));
+				}
+			}
+		}
+		
+		for (int j=0; j<i; j++) {  //v
+        	
+            	System.out.println("msgContentList " + msgContentList.get(j));
+        }
+        
+	}
+}
+	
